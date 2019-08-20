669504597ecc6272da17c7c9c813e74998fbf00c
quanghung112 <laiquanghung112@gmail.com>
2019-08-20 08:40:09 +0700

    create database

---
+++ database/seeds/UserSeederTable.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeederTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=new \App\User();
+        $user->username='quanghung';
+        $user->password= \Illuminate\Support\Facades\Hash::make(123456);
+        $user->email='laiquanghung199x@gmail.com';
+        $user->save();
+    }
+}
