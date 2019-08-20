eb0c2b5f9c061d5d966459ccdf218c1d322b3246
quanghung112 <laiquanghung112@gmail.com>
2019-08-20 11:44:08 +0700

    Hung push login/logout

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', 'AuthController@login');
+Route::group(['middleware' => 'jwt.verify'], function () {
+    Route::post('/logout', 'AuthController@logout');
 });
